907ea3d8bb7e28f7a38f0935c941d7627b67de8e
Amar <amar@capital-match.com>
2016-02-06 20:10:18 +0800

    added helper functions

--- src/DockerCompose.hs
+++ src/DockerCompose.hs
@@ -4,5 +4,14 @@ module DockerCompose
     , module DockerCompose.Generator
     ) where
 
+import           Control.Monad.Writer    (runWriter)
+import           Data.Proxy
 import           DockerCompose.API
 import           DockerCompose.Generator
+import           System.IO
+
+toFile :: (HasGenerator a) => FilePath -> Proxy a -> Handler a -> IO ()
+toFile file p = writeFile file . snd . runWriter . generate p
+
+asString :: (HasGenerator a) => Proxy a -> Handler a -> String
+asString p = snd . runWriter . generate p
