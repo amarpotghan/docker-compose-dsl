13f55d1d3fba8fe201f690711df444a5fd6c1583
Amar <amar@capital-match.com>
2016-02-12 21:58:58 +0800

    formatting generate functions

--- src/DockerCompose/Generator.hs
+++ src/DockerCompose/Generator.hs
@@ -26,36 +26,49 @@ class HasGenerator a where
 
 instance (HasGenerator a, HasGenerator b) => HasGenerator ( a :&: b ) where
   type Arg (a :&: b) =  Arg a :&: Arg b
-  generate _ ( a :&: b) = generate (Proxy :: Proxy a) a *> generate (Proxy :: Proxy b) b
+  generate _ ( a :&: b) = generate (Proxy :: Proxy a) a
+                          *> generate (Proxy :: Proxy b) b
 
 instance (HasGenerator rest, KnownSymbol current) => HasGenerator (current :~ rest) where
   type Arg (current :~ rest) = Arg rest
-  generate _ restArg = modify (++ [(single . L.pack . symbolVal $ (Proxy :: Proxy current))]) *> generate (Proxy :: Proxy rest) restArg
+  generate _ restArg = modify addContainer
+                       *> generate (Proxy :: Proxy rest) restArg
+    where
+      addContainer = (++ [single $ L.pack (symbolVal (Proxy :: Proxy current))])
 
 instance (HasGenerator a, HasGenerator b) => HasGenerator (a :& b) where
   type Arg (a :& b) = Arg a :& Arg b
-  generate _ ( a :& b) = generate (Proxy :: Proxy a) a *> generate (Proxy :: Proxy b) b
+  generate _ ( a :& b) = generate (Proxy :: Proxy a) a
+                         *> generate (Proxy :: Proxy b) b
   
 instance (KnownSymbol a, Show value, ToStringy s) => HasGenerator (s := Capture a value) where
   type Arg (s := Capture a value) = value
-  generate _ v = modify $ appendProperty $ SingleVal (KeyRep . L.pack $ toStringy (Proxy :: Proxy s))  (ValueRep . L.pack . show $ v) 
+  generate _ v = modify $ appendProperty $ SingleVal k value where
+    k = KeyRep $ L.pack (toStringy (Proxy :: Proxy s))
+    value = ValueRep $ L.pack (show v)
     
 instance (Show a, ToStringy s) => HasGenerator (s := Value a) where
   type Arg (s := Value a) = a
-  generate _ v = modify $ appendProperty $ SingleVal (KeyRep . L.pack $ toStringy (Proxy :: Proxy s))  (ValueRep . L.pack . show $ v) 
+  generate _ v = modify $ appendProperty $ SingleVal k value where
+    k = KeyRep $ L.pack (toStringy (Proxy :: Proxy s))
+    value = ValueRep $ L.pack (show v)
 
 instance (KnownSymbol a, ToStringy s) => HasGenerator (s := a) where
   type Arg (s := a) = ()
-  generate _ _ = modify $ appendProperty $ SingleVal (KeyRep . L.pack $ toStringy (Proxy :: Proxy s))  (ValueRep . L.pack $ symbolVal (Proxy :: Proxy a)) 
+  generate _ _ = modify $ appendProperty (SingleVal k v) where
+    k = KeyRep . L.pack $ toStringy (Proxy :: Proxy s)
+    v = ValueRep . L.pack $ symbolVal (Proxy :: Proxy a)
 
 instance (Show a, ToStringy s) => HasGenerator (s := Values a) where
   type Arg (s := Values a) = [ a ]
-  generate _ v = modify $ appendProperty $ PlainList (KeyRep $ L.pack $ toStringy (Proxy :: Proxy s)) (ValueRep . L.pack . show <$> v)
+  generate _ v = modify $ appendProperty $ PlainList k values where
+    k = KeyRep $ L.pack (toStringy (Proxy :: Proxy s))
+    values = ValueRep . L.pack . show <$> v
 
 instance (KnownSymbol a, ToStringy s, HasGenerator rest) => HasGenerator (s := (a ': rest)) where
    type Arg (s := (a ': rest)) = Arg (a ': rest)
    generate _ v = do
-     modify $ appendProperty $ PlainList (KeyRep $ L.pack $ toStringy (Proxy :: Proxy s)) []
+     modify $ appendProperty $ PlainList (KeyRep $ L.pack (toStringy (Proxy :: Proxy s))) []
      generate (Proxy :: Proxy (a ': rest)) v
      
 instance (KnownSymbol a, HasGenerator rest) => HasGenerator (a ': rest) where
