cf2c95f8be6843f92b37bbdb806450ba30d7d092
Amar <amar@capital-match.com>
2016-08-17 23:49:30 +0800

    Some more whitespace cleanup

--- test/DockerComposeSpec.hs
+++ test/DockerComposeSpec.hs
@@ -8,55 +8,63 @@ import           Data.Proxy
 import           DockerCompose
 import           Test.Hspec
 
-type NameBinder = "app" :~
-                    Image := "something"
+type NameBinder =
+  "app" :~
+  Image := "something"
 
-type PlainListTest = "app" :~
-                       VolumesFrom := ["cont1", "cont2", "cont3"]
+type PlainListTest =
+  "app" :~
+  VolumesFrom := ["cont1", "cont2", "cont3"]
 
-type SingleArg = "app" :~
-                   Image := Value String
+type SingleArg =
+  "app" :~
+  Image := Value String
 
-type LeftGroup = ("app" :~ Image := "somthing" :&: "mail" :~ Image := "else") :&: "bank" :~ Image := "other"
-type RightGroup = "app" :~ Image := "somthing" :&: ("mail" :~ Image := "else" :&: "bank" :~ Image := "other")
+type LeftGroup =
+  ("app" :~
+   Image := "somthing" :&:
+   "mail" :~
+   Image := "else"
+  ) :&:
+  "bank" :~
+  Image := "other"
 
-type PropertyLeftGroup = "app" :~ (Image := "somthing" :& Environment := "env") :& VolumesFrom := "cont1"
+type RightGroup =
+  "app" :~
+  Image := "somthing" :&:
+  ("mail" :~
+   Image := "else" :&:
+   "bank" :~
+   Image := "other")
 
-type PropertyRightGroup = "app" :~ Image := "somthing" :& (Environment := "env" :& VolumesFrom := "cont1")
+type PropertyLeftGroup =
+  "app" :~
+  (Image := "somthing" :& Environment := "env") :&
+  VolumesFrom := "cont1"
+
+type PropertyRightGroup =
+  "app" :~
+  Image := "somthing" :&
+  (Environment := "env" :& VolumesFrom := "cont1")
 
 dockerComposeSpec :: Spec
 dockerComposeSpec = describe "Specifications" $ do
-
   describe "Generator specs" $ do
     let createTree p =  execDockerCompose mempty . generate p
-
     it "on (:~) should add new node to the tree" $ do
       composeTree <- createTree (Proxy :: Proxy NameBinder) NoArg
       head composeTree `shouldBe` ContainerNode (NodeName "app") [SingleVal (KeyRep "image") (ValueRep "something")]
-
     it "on a type level list of strings should add PlainList to the tree" $ do
       composeTree <- createTree (Proxy :: Proxy PlainListTest) NoArg
       (nodeProperties . head $ composeTree) `shouldBe` [ PlainList (KeyRep "volumes_from") $ ValueRep <$> ["cont1", "cont2", "cont3"] ]
-
     it "accepts arguments of specified type" $ do
       composeTree <- createTree (Proxy :: Proxy SingleArg) "TestArg"
       (nodeProperties . head $ composeTree) `shouldBe` [ SingleVal (KeyRep "image") (ValueRep "\"TestArg\"")]
-
     it ":&: is associative" $ do
       composeTree1 <- createTree (Proxy :: Proxy LeftGroup) ((NoArg :&: NoArg) :&: NoArg)
       composeTree2 <- createTree (Proxy :: Proxy RightGroup) (NoArg :&: (NoArg :&: NoArg))
-
       composeTree1 `shouldBe` composeTree2
-
     it ":& is associative" $ do
       composeTree1 <- createTree (Proxy :: Proxy PropertyLeftGroup) ((NoArg :& NoArg) :& NoArg)
       composeTree2 <- createTree (Proxy :: Proxy PropertyRightGroup) (NoArg :& (NoArg :& NoArg))
-
       composeTree1 `shouldBe` composeTree2
-
-
-
-
-
-
-
